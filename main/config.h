f25fa16147a1ee12c018203d99b734f4aaa6461c
Derick Kunz <derick.kunz@gmail.com>
2025-07-26 03:56:16 -0300

    LapespSensor funcional main.ino experimental

---
+++ main/config.h
@@ -0,0 +1,23 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//ENDEREÇOS 
+#define BME_ADDRESS 0x76
+#define MPU_ADDRESS 0x68
+#define MPU_BMP_ADDRESS 0x77/0x68
+
+//GPIO, PINOUT, PINS
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+//CONSTS 
+#define SEALEVELPRESSURE_HPA (1013.25) 
+
+//CONFIGS
+#define SAMPLES_PER_SECOND 10
+#define BME_LOGGER "(BME)"
+#define GY91_MPU_LOGGER "(MPU)"
+#define GY91_BMP_LOGGER "(BMP)"
+
+//END DEFINE
+#endif
---
+++ main/main.ino
@@ -0,0 +1,107 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_BMP280.h>
+#include <MPU9250_asukiaaa.h>
+#include "config.h"
+
+Adafruit_BME280 bme; // I2C
+Adafruit_BMP280 bmp; // I2C
+MPU9250_asukiaaa mySens;
+
+float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ, bmpInitialPressure, bmeInitialPressure;
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  //BME280 SETUP
+  bme.begin(BME_ADDRESS, &Wire);  
+  bmeInitialPressure = bmp.readPressure();
+
+  //GY-91 (MPU9250 e BMP280) SETUP 
+  Wire.begin(SDA_PIN, SCL_PIN);
+  mySens.setWire(&Wire);
+  mySens.beginAccel();
+  mySens.beginGyro();
+  mySens.beginMag();
+
+  bmp.begin(0x76);
+  bmpInitialPressure = bmp.readPressure();
+}
+
+void loop() 
+{ 
+  //BME Handler
+  bmeHandler();
+  
+  //GY-91 Handler
+  gyHandler();
+  Serial.println();
+
+  //Controle de ciclo
+
+}
+
+
+void bmeHandler() 
+{
+  Serial.print(BME_LOGGER);
+  Serial.print("%s Temperature: ");
+  Serial.print(bme.readTemperature());
+  Serial.print(" *C\t");
+  
+  Serial.print(BME_LOGGER);
+  Serial.print("Pressure: ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print(" hPa\t");
+  
+  Serial.print(BME_LOGGER);
+  Serial.print("Approx. Altitude: ");
+  Serial.print(bme.readAltitude(bmeInitialPressure));
+  Serial.println(" m\t");
+  
+  Serial.print(BME_LOGGER);
+  Serial.print("Humidity: ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %\t");
+}
+
+void gyHandler()
+{
+  Serial.print(GY91_BMP_LOGGER + "Temperature(°C): " + String(bmp.readTemperature() - 5.0));
+  Serial.print(GY91_BMP_LOGGER + "Pressure: " + String(bmp.readPressure() / 100));
+  Serial.print(GY91_BMP_LOGGER + "Altitude: " + String(readAltitude(bmpInitialPressure)));
+  Serial.print(GY91_MPU_LOGGER);
+  if (mySens.accelUpdate() == 0) {
+    aX = mySens.accelX();
+    aY = mySens.accelY();
+    aZ = mySens.accelZ();
+    aSqrt = mySens.accelSqrt();
+    Serial.print("accelX: " + String(aX));
+    Serial.print("accelY: " + String(aY));
+    Serial.print("accelZ: " + String(aZ));
+    Serial.print("accelSqrt: " + String(aSqrt));
+  }
+  Serial.print(GY91_MPU_LOGGER);
+  if (mySens.gyroUpdate() == 0) {
+    gX = mySens.gyroX();
+    gY = mySens.gyroY();
+    gZ = mySens.gyroZ();
+    Serial.print("gyroX: " + String(gX));
+    Serial.print("gyroY: " + String(gY));
+    Serial.print("gyroZ: " + String(gZ));
+  }
+    Serial.print(GY91_MPU_LOGGER);
+  if (mySens.magUpdate() == 0) {
+    mX = mySens.magX();
+    mY = mySens.magY();
+    mZ = mySens.magZ();
+    mDirection = mySens.magHorizDirection();
+    Serial.print("magX: " + String(mX));
+    Serial.print("magY: " + String(mY));
+    Serial.print("magZ: " + String(mZ));
+    Serial.print("horizontalDirection: " + String(mDirection));
+  }
+}
