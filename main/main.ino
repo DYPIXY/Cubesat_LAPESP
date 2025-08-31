a0993bda71a9a593a6af1a1fb8ea82365be1ba99
Derick Kunz <derick.kunz@gmail.com>
2025-08-30 21:33:58 -0300

    classe lapesp

--- main/main.ino
+++ main/main.ino
@@ -6,11 +6,33 @@
 #include <MPU9250_asukiaaa.h>
 #include "config.h"
 
-Adafruit_BME280 bme; // I2C
-Adafruit_BMP280 bmp; // I2C
+//Objeto para envio
+struct esp2esp
+{
+  //BME
+  double BME_TEMP;
+  double BME_PRESSURE;
+  double BME_ALTITUDE;
+  double BME_HUMIDITY;
+
+  //GY MPU9250
+  double GY91_BMP_TEMP;
+  double GY91_BMP_PRESSURE;
+  double GY91_BMP_ALTITUDE;
+  double GY91_BMP_;
+
+  //GPS
+  double GPS_LAT;
+  double GPS_LONG;
+  int GPS_COUNT
+};
+
+
+Adafruit_BME280 bme; 
+Adafruit_BMP280 bmp; 
 MPU9250_asukiaaa mySens;
 
-float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ, bmpInitialPressure, bmeInitialPressure;
+static float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ, bmpInitialPressure, bmeInitialPressure;
 
 void setup() 
 {
